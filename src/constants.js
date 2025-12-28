61d26f2ff7e2d07399e9b2c6add73914e371ccbd
Dan Zaharia <zaharia.danny@gmail.com>
2025-12-28 09:37:42 -0600

    Increase collision buffer for better ergonomics

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const TILE_SIZE = 50; // pixels
 export const PLAYER_SPEED = 2; // pixels per frame (must be a divisor of TILE_SIZE for smooth grid alignment)
-export const PLAYER_COLLISION_BUFFER = 5; // pixels to inset from the tile edges for collision detection
+export const PLAYER_COLLISION_BUFFER = 16; // pixels to inset from the tile edges for collision detection
 
 // 0 = empty floor
 // 1 = red wall
